bc60fb0130f10872b4e6538d8d1d6339b9818dc8
Sennzai <sennzaitopup@gmail.com>
2024-10-27 12:58:37 +0700

    autoscrape and check proxies

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def scrape():
 def check_proxy(proxy):
     try:
         # Use a simple GET request to check the proxy
-        response = requests.get('https://api.ipify.org/', proxies={"http": proxy, "https": proxy}, timeout=5)
+        response = requests.get('http://api.ipify.org/', proxies={"http": proxy, "https": proxy}, timeout=5)
         if response.status_code == 200:
             return True
         else:
@@ -27,13 +27,8 @@ def check_proxy(proxy):
         return False
 
 if __name__ == "__main__":
-    # Read the list of proxies from a file
-    # with open("list.txt", "r") as f:
-    #     proxies = f.read().splitlines()
-    
-    # for proxy in proxies:
-    #     if check_proxy(proxy):
-    #         print(f"[LIVE] {proxy}")
-    #     else:
-    #         print(f"[DEAD] {proxy}")
-    print(scrape())
+    for proxy in scrape():
+        if check_proxy(proxy):
+            print(f"[LIVE] {proxy}")
+        else:
+            print(f"[DEAD] {proxy}")
